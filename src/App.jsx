20e9bf81e5a241d8c9ac951283ea852de513bc7a
siddharth-mehra <sid15.mehra@gmail.com>
2024-10-11 18:17:12 +0530

    Form is Created

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,15 @@ const App = () => {
   return (
     <div className="App">
       <h1>Gen Ai Application</h1>
+
+      <div className='form-container'>
+        <form className='Query'>
+          <label>Enter Query</label>
+          <input className='query-input' type="text" placeholder="Enter query"/>
+        </form>
+      </div>
     </div>
   )
 }
 
-export default App
+export default App;
